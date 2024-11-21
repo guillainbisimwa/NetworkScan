aba611f487d183d79bed7a31cd55e0d1e231f824
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-11-21 17:25:43 +0200

    Remove Combined fragment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,8 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_bluetooth, R.id.navigation_dashboard,  R.id.navigation_cellular,
-                R.id.navigation_combined_data
+                R.id.navigation_bluetooth, R.id.navigation_dashboard,  R.id.navigation_cellular
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
